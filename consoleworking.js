dee954eb8e7e9c661278256a3d1bc8667e3f66a6
prateek951 <prince.cenation@gmail.com>
2017-12-31 21:16:17 +0530

    working with console

---
+++ consoleworking.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello'
+console.log(greeting);
+console.log([1,'e',2,3,true]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+console.error('This is some error');
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert('File Linked!!');
+
